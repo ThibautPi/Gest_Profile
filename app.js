3249e98c693036a6d2a0c47540948560255520d5
tpiquard <thibaut.piquardr@student.uclouvain.be>
2019-04-23 15:14:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.set('useCreateIndex', true);
+mongoose.connect('mongodb://localhost/APIProfile', {useNewUrlParser: true});
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/profile',require('./routes/profile'));
+
+// Start the server
+const port = process.env.PORT || 5003;
+app.listen(port);
+console.log('Server listening at '+port);
